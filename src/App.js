d2ad6a4f73087a98817b05d8aaac4a7d6d6ff4cd
Brian <e.kallensp@gmail.com>
2021-05-04 00:48:44 -0400

    Cards with real data added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.scss';
+import { fetchData } from './api';
 
 const App = () => {
+
+    const [ data, setData ] = useState({});
+
+    useEffect(() => {
+
+        const obtenerData = async() => {
+            const resp = await fetchData();
+            setData(resp);
+        }
+
+        obtenerData();
+
+    }, []);
+
+
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Covid Tracker</h1>
+            <Cards data={data}/>
+            <CountryPicker />
+            <Chart />
         </div>
     )
 }
